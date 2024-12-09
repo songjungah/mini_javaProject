4c175bcb1486f7c16d952d52b043fe573b6b852f
kmskmk21 <kmskmk21@gmail.com>
2024-12-09 21:47:55 +0900

    241205 kimminseon_view

--- src/model/MemberDao.java
+++ src/model/MemberDao.java
@@ -1,6 +1,52 @@
 package model;
 
-public class MemberDao
-{
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+// 회원 데이터 db와 연동시켜 crud 작업 수행
+public class MemberDao {
+    // db 연결 정보
+
+    // db 연결 메서드
+    private Connection conn; // Dao 클래스 Connection 공유
+
+    // MemeberDao 생성자
+    public MemberDao() {
+        Dao dao = new Dao(); // dao 객체 생성
+        this.conn = dao.conn; // Connection 객체 가져옴
+    }
+
+    // 회원 데이터 삽입
+    public void insertMember(MemberDto member) {
+        String sql = "INSERT INTO member (uNumber, uPw,uId, uPw, uName, uAge, uAdd, uEmail, uPhone, uBirth)" + "VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+    try(PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        pstmt.setInt(1,member.getuNumber());
+        pstmt.setString(2,member.getuId());
+        pstmt.setString(3, member.getuPw());
+        pstmt.setString(4, member.getuName());
+        pstmt.setInt(5, member.getuAge());
+        pstmt.setString(6, member.getuAdd());
+        pstmt.setString(7, member.getuEmail());
+        pstmt.setString(8, member.getuPhone());
+        pstmt.setString(9, member.getuBirth());
+
+        pstmt.executeUpdate();
+        System.out.println("회원 정보가 성공적으로 삽입되었습니다");
+    } catch (SQLException e) {
+        e.printStackTrace();
+        System.err.println("회원 정보 삽입 오류 발생" + e.getMessage());
+    }
+    }
+
+    // 회원 데이터 조회
+    public  MemberDto getMember(String uId){
+        String sql = "SELECT * FROM member WHERE uId = ?";
+        MemberDto member = null;
+
+        try(PreparedStatement pstmt = conn.prepareStatement())
+    }
+
+
 
 }
--- src/model/MemberDto.java
+++ src/model/MemberDto.java
@@ -30,6 +30,93 @@ public class MemberDto {
     }
 
     // getter & setter 메서드 (데이터 읽고 쓰기 위한 접근자 메서드)
+    // 회원 번호 getter
+    public int getuNumber() {
+        return uNumber;
+    }
 
+    // 회원 번호 setter
+    public void setuNumber(int uNumber) {
+        this.uNumber = uNumber;
+    }
 
+    // 회원 아이디 getter
+    public String getuId() {
+        return uId;
+    }
+
+    // 회원 아이디 setter
+    public void setuId(String uId) {
+        this.uId = uId;
+    }
+
+    // 회원 비번 getter
+    public String getuPw() {
+        return uPw;
+    }
+
+    // 회원 비번 setter
+    public void setuPw(){
+        this.uPw = uPw;
+    }
+
+    // 회원 이름 getter
+    public String getuName() {
+        return uName;
+    }
+
+    // 회원 이름 setter
+    public void setuName() {
+        this.uName = uName;
+    }
+
+    // 회원 나이 getter
+    public int getuAge() {
+        return uAge;
+    }
+
+    // 회원 나이 setter
+    public void setuAge() {
+        this.uAge = uAge;
+    }
+
+    // 회원 주소 getter
+    public String getuAdd() {
+        return uAdd;
+    }
+
+    // 회원 주소 setter
+    public void setuAdd() {
+        this.uAdd = uAdd;
+    }
+
+    // 회원 이메일 getter
+    public String getuEmail() {
+        return uEmail;
+    }
+
+    // 회원 이메일 setter
+    public void setuEmail() {
+        this.uEmail = uEmail;
+    }
+
+    // 회원 전화번호 getter
+    public String getuPhone() {
+        return uPhone;
+    }
+
+    // 회원 전화번호 setter
+    public void setuPhone() {
+        this.uPhone = uPhone;
+    }
+
+    // 회원 생년월일 getter
+    public String getuBirth() {
+        return uBirth;
+    }
+
+    // 회원 생년월일 setter
+    public void setuBirth() {
+        this.uBirth = uBirth;
+    }
 }
