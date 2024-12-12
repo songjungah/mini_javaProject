3b65e6d09b931eeeab40dc886130b553b1a0c14f
kmskmk21 <kmskmk21@gmail.com>
2024-12-12 21:49:48 +0900

    영화 추천 관련 코드인데 전체적으로 봐야할거 같습니다

--- src/controller/MovieController.java
+++ src/controller/MovieController.java
@@ -22,4 +22,25 @@ public class MovieController {
 
     }   // e end
 
+
+
+    // 추천 영화 관련 추가 내용입니다
+
+    // 추천된 영화 가져오기
+    public ArrayList<MovieDto> getRecommendedMovies() {
+        return MovieDao.getInstance().RecommendedMovies();
+    }
+
+    // 특정 리뷰를 추천으로 표시
+    public boolean recommendReview(int reviewId) {
+        return MovieDao.getInstance().markMovieAsRecommended(reviewId);
+    }
+
+    // 추천 영화 가져오기
+    public ArrayList<MovieDto> recommendMovies( String genre, String director, String actor, String movieClass) {
+        return MovieDao.getInstance().getRecommendedMovies(genre, director, actor, movieClass);
+    }
+
+
+
 }
